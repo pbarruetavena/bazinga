7047bfddbb187c1854bc1e2434cdbd419242b845
PedrGBarruetavenaV <pbarruetavenavieira@gmail.com>
2022-10-24 22:36:00 -0300

    mudancas

---
+++ funcionalidades-principais.js
@@ -0,0 +1,27 @@
+const botoesOpcaoArr = document.querySelectorAll('.botao-opcao');
+
+
+
+// animação no botão
+
+function revelaTextoBotao(e){
+    let botao = e.currentTarget;
+    let paragrafo = document.createElement('p');
+
+    paragrafo.innerHTML = botao.dataset.texto;
+    botao.classList.add('botao-extendido');
+    botao.appendChild(paragrafo);
+}
+function escondeTextoBotao(e){
+    let botao = e.currentTarget;
+    let paragrafo = document.querySelector('.botao-extendido p');
+    botao.classList.remove('botao-extendido');
+    botao.removeChild(paragrafo);
+}
+
+for(let i = 0; i < 3; i++){
+    botoesOpcaoArr[i].addEventListener('mouseover', revelaTextoBotao);
+}
+for(let i = 0; i < 3; i++){
+    botoesOpcaoArr[i].addEventListener('mouseout', escondeTextoBotao);
+}
