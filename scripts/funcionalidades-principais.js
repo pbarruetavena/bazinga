05f90c5ccfb8173abfa811f667dab7da12297bb3
PedrGBarruetavenaV <pbarruetavenavieira@gmail.com>
2022-10-29 20:11:45 -0300

    estilizacao do ranking

--- scripts/funcionalidades-principais.js
+++ scripts/funcionalidades-principais.js
@@ -1,10 +1,17 @@
 //  Inicializa os menus de configuração e ranking com display none
-document.querySelector('#configuracao').style.display = 'none';
-document.querySelector('#secao-ranking').style.display = 'none';
+$('#secao-ajuda').css({display: "none"});
+$('#secao-ranking').css({display: "none"});
+$('#configuracao').css({display: "none"});
 
 
 let menuSelecionado = 'principal';  // qual menu está selecionado
 
+/*
+        $('#secao-ajuda').css({display: "none"});
+        $('#secao-ranking').css({display: "none"});
+        $('#principal').css({display: "flex"});
+        $('#configuracao').css({display: "none"});
+*/
 
 function mostraConf(){
     if(menuSelecionado != 'configuracao'){
@@ -24,6 +31,24 @@ function mostraConf(){
     }
 }
 
+function mostraRanking(){
+    if(menuSelecionado != 'ranking'){
+        $('#secao-ajuda').css({display: "none"});
+        $('#secao-ranking').css({display: "block"});
+        $('#principal').css({display: "none"});
+        $('#configuracao').css({display: "none"});
+
+        menuSelecionado = 'ranking';
+    } else {
+        $('#secao-ajuda').css({display: "none"});
+        $('#secao-ranking').css({display: "none"});
+        $('#principal').css({display: "flex"});
+        $('#configuracao').css({display: "none"});
+
+        menuSelecionado = 'principal';
+    }
+}
+
 
 
 //              --------------------- BOTÕES -------------------------------
@@ -31,9 +56,12 @@ $('#icone-sobre').click(() => open('sobre-nos.html', '_blank'));
 
 $('#icone-configuracao').click(mostraConf);
 $('#secao-perfil').click(mostraConf);
+$('#ranking').click(mostraRanking);
+$('#fechar-ranking').click(mostraRanking);
 
 $('.botao-opcao:first-child').click( () => location.href = 'jogo/jogo.html');
 
 
 //teste --- descomentar para desenvolver o menu de configurações
 //mostraConf();
+mostraRanking();
