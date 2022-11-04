479753b338c073b5d99de389d4c19569d8e90905
PedrGBarruetavenaV <pbarruetavenavieira@gmail.com>
2022-11-04 15:20:02 -0300

    consertei algumas coisas

--- scripts/sobre-nos.js
+++ scripts/sobre-nos.js
@@ -1,2 +1,7 @@
 
 $('#botao-home').click( () => location.href = "index.html");
+
+/* 
+ * Ideias de easter egg: botão de curtir nas cartas que desbloquearia uma imagem de perfil especial (carta do site), dicas especiais e temas pré-definidos
+ * Funcionamento do sobre nós: Cartas em uma "roleta", uma selecionada mostra tudo que tem, as laterais são clicáveis e mostram somente uma letra, e quando clicadas elas se tornam a carta selecionada
+*/
