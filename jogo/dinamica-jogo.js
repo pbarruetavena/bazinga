669265cf627c20e36648059a0f9362cc7eca4f58
PedrGBarruetavenaV <pbarruetavenavieira@gmail.com>
2022-11-12 20:18:26 -0300

    n aguento mais nada da certo

--- jogo/dinamica-jogo.js
+++ jogo/dinamica-jogo.js
@@ -1,6 +1,24 @@
+let cartasEl = document.querySelectorAll('.carta');
+
+
+for(let carta of cartasEl) {
+    carta.addEventListener('click', (e) => {
+        carta.style.position = "absolute";
+        carta.style.left = "20" + "vw";
+        carta.style.top = "20" + "vh";
+    });
+}
+
 
 function pause() {
     
 }
 
 $('#icone-pause').click(pause);
+
+posTemaAtual = storage.carregarPosTemaAtual();
+temas = storage.carregarTemas();
+temaAtual = temas[posTemaAtual];
+
+atualiza.tema(temaAtual);
+atualiza.perfil(perfis[perfilAtual]);
