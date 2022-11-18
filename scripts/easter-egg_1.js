6c351cb7ba45ec34149ce31a2b4bd9db2164e490
VictorN77 <108091222+VictorN77@users.noreply.github.com>
2022-11-18 13:32:00 -0300

    Update easter-egg_1.js

--- scripts/easter-egg_1.js
+++ scripts/easter-egg_1.js
@@ -1,1 +1,11 @@
-$('#voltar').click(() => location.href  = 'sobre-nos.html');
+//$('#voltar').click(() => location.href  = 'sobre-nos.html');
+
+let indEl = document.querySelector('#indicador');
+let martEl = document.querySelector('#martelo');
+
+martEl.addEventListener('click', function(){
+    let rect = indEl.getBoundingClientRect();
+
+
+});
+
