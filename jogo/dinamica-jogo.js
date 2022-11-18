92d8e8c909b91c710ed7706306bdac54d88b26f0
PedrGBarruetavenaV <pbarruetavenavieira@gmail.com>
2022-11-18 20:39:19 -0300

    embaralhar cartas vetor

--- jogo/dinamica-jogo.js
+++ jogo/dinamica-jogo.js
@@ -56,6 +56,56 @@ function jogadorVencedor(cartaJogador, valorJogador, cartaBot, valorBot){
 
 }
 
+function gera_int_exlusive(limite){
+    return Math.floor(Math.random() * limite);
+}
+
+function compara_carta(c1, c2){
+    if(c1.valor == c2.valor){
+        if(c1.naipe == c2.naipe){ return 1; }
+    }
+    return 0;
+}
+
+function embaralha(){
+    let naipes = ['R', 'P', 'T', 'L', 'S'];
+    let valores = ['A', 2, 3, 4, 5, 6, 7];
+
+    let naipe;
+    let valor;
+
+    let cartas_mao = [];
+
+    for(let i = 0; i < 8; i++){
+
+        let aux;
+        while(true){
+            aux = true;
+            naipe = naipes[gera_int_exlusive(naipes.length)];
+            valor = valores[gera_int_exlusive(valores.length)];
+            let cartaObj = {
+                naipe: naipe,
+                valor: valor
+            };
+
+            for(let j = 0; j < cartas_mao.length; j++){
+                if(compara_carta(cartaObj, cartas_mao[j])){
+                    aux = false;
+                    break;
+                }
+            }
+
+            if(aux == true){
+                cartas_mao.push(cartaObj);
+                break;
+            }
+        }
+
+    }
+    
+    return cartas_mao;
+}
+
 
 for(let carta of cartasEl) {
     carta.addEventListener('click', (e) => {
