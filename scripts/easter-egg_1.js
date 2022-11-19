058491a8bd6919db9e15109d39b64bf352ef8db3
unknown <victornpando.ferreira@outlook.com>
2022-11-19 17:22:47 -0300

    alterções na animação do martelo

--- scripts/easter-egg_1.js
+++ scripts/easter-egg_1.js
@@ -11,8 +11,10 @@ marteloEl.addEventListener('click', function(){
 
 //  Alterna classe quando clicado o martelo
 //  Mudei o nome pra marteloEl, ao invés de martEl
-
+//  Martelo tira classe por conta própria
+//  correções no timing da animação (1000ms = melhor ângulo)
 marteloEl.addEventListener('click', function(e){
     marteloEl.classList.toggle('marteloClicado');
+    setTimeout(function(){marteloEl.classList.toggle('marteloClicado');}, 1000);
 });
 
