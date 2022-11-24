3f035756242106c9c83f17f985c3d4816f1e1872
unknown <victornpando.ferreira@outlook.com>
2022-11-24 20:26:24 -0300

    função de identificar altura do elem e outras alterações pequenas

--- scripts/easter-egg_1.js
+++ scripts/easter-egg_1.js
@@ -2,10 +2,21 @@
 
 let indEl = document.querySelector('#indicador');
 let marteloEl = document.querySelector('#martelo');
+let resultEl = document.querySelector("#result");
 
 marteloEl.addEventListener('click', function(){
     let rect = indEl.getBoundingClientRect();
+    let alt = rect.top;
+    console.log(alt);
 
+    if(alt <= 530)
+        resultEl.textContent = "MONSTRO!";
+    else if (alt <= 560)
+        resultEl.textContent = "Gorila!";
+    else if (alt <= 590)
+        resultEl.textContent = "fisiculturista";
+    else if (620 <= 620)
+        resultEl.textContent = "lenhador";
 
 });
 
@@ -16,5 +27,7 @@ marteloEl.addEventListener('click', function(){
 marteloEl.addEventListener('click', function(e){
     marteloEl.classList.toggle('marteloClicado');
     setTimeout(function(){marteloEl.classList.toggle('marteloClicado');}, 1000);
+    indEl.classList.toggle('parar');
+    setTimeout(function(){indEl.classList.toggle('parar');}, 1000);
 });
 
