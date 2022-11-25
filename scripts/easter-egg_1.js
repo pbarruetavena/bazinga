1165c8333403a6b2f2a69c42bdfe9b2a05f5f74b
unknown <victornpando.ferreira@outlook.com>
2022-11-24 21:21:55 -0300

    função de identificar altura responsiva agora

--- scripts/easter-egg_1.js
+++ scripts/easter-egg_1.js
@@ -6,18 +6,41 @@ let resultEl = document.querySelector("#result");
 
 marteloEl.addEventListener('click', function(){
     let rect = indEl.getBoundingClientRect();
-    let alt = rect.top;
+    let alt = (rect.top / window.innerHeight) * 100; 
     console.log(alt);
 
-    if(alt <= 530)
+    if(alt <= 43){
         resultEl.textContent = "MONSTRO!";
-    else if (alt <= 560)
+        document.getElementById("result").style.color = 'red';
+    }
+    else if (alt <= 46){
         resultEl.textContent = "Gorila!";
-    else if (alt <= 590)
+        document.getElementById("result").style.color = 'rgb(24, 71, 37)';
+    }
+    else if (alt <= 49){
         resultEl.textContent = "fisiculturista";
-    else if (620 <= 620)
+        document.getElementById("result").style.color = 'rgb(171, 115, 19)';
+    }
+    else if (alt <= 52){
         resultEl.textContent = "lenhador";
-
+        document.getElementById("result").style.color = 'rgb(171, 168, 19)';
+    }
+    else if (alt <= 55){
+        resultEl.textContent = "spock";
+        document.getElementById("result").style.color = 'rgb(19, 153, 171)';
+    }
+    else if (alt <= 58){
+        resultEl.textContent = "lagarto";
+        document.getElementById("result").style.color = 'rgb(119, 133, 120)';
+    }
+    else if (alt <= 60){
+        resultEl.textContent = "pedra";
+        document.getElementById("result").style.color = 'rgb(61, 64, 61)';
+    }
+    else {
+        resultEl.textContent = "papel";
+        document.getElementById("result").style.color = 'rgb(247, 247, 247)';
+    }
 });
 
 //  Alterna classe quando clicado o martelo
